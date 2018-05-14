d81c58b05bdf89045d52a3eb29dd33e654453079
van <463912679@qq.com>
2018-05-14 21:56:55 +0800

    添加thread_idle文件

---
+++ thread_idle.cpp
@@ -0,0 +1,17 @@
+#include "thread_idle.h"
+using namespace std;
+thread_idle tIdle;
+thread_idle::thread_idle()
+{
+Idle[FPGA_FOUR_CN]=true;
+Idle[SUB_CN]=false;
+Idle[MAIN_CN]=false;
+Idle[MVDECT_CN]=false;
+Idle[FPGA_SIX_CN]=true;
+}
+bool thread_idle::isToIdle(int idx){
+	return Idle[idx];}
+		void thread_idle::threadIdle(int idx){
+			Idle[idx]=true;};
+		void thread_idle::threadRun(int idx){
+			Idle[idx]=false;};
