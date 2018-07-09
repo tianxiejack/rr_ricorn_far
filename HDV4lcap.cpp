40e0052bd09d76157c5c4bdffa11a8a875672a44
van <463912679@qq.com>
2018-07-09 18:12:41 +0800

    存图%.2d

--- HDV4lcap.cpp
+++ HDV4lcap.cpp
@@ -791,7 +791,7 @@ int HDv4l_cam::read_frame(int now_pic_format)
 									if(saveSinglePic[i]==true)
 									{
 										saveSinglePic[i]=false;
-										sprintf(filename,"%2d.bmp",i);
+										sprintf(filename,"%.2d.bmp",i);
 										save_single_pic(filename,(unsigned char *)buffers[buf.index].start,nowpicW,nowpicH);
 									}
 								}
